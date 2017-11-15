d6191657df46d73a64e7c97f09741f5debab1ea6
rodolfodisalvo <rodidisalvo@hotmail.com>
2017-11-15 01:37:32 -0300

    primer prueba voto

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -6,13 +6,17 @@ class CarsController < ApplicationController
       question_id = params[:question_id]
 
       if params[:votoN]=="1"
-
+      	voto=QuestionVote.new(user_id: session[:user_id],question_id: question_id,tipo: false)
       end
-
       if params[:votoP]=="1"
-
+      	voto=QuestionVote.new(user_id: session[:user_id],question_id: question_id,tipo: true)
       end
-
+      if voto.save
+      	flash[:message]="Voto con exito"
+      else
+      	flash[:message]="Error en el voto"
+  	  end
+  	  redirect_to "#{questions_path}/#{question_id}"
 
   end
 
