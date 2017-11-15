b5373811aea6ac69aec0592a6c70e3be08a308a9
rodolfodisalvo <rodidisalvo@hotmail.com>
2017-11-15 00:21:13 -0300

    A arreglar

---
+++ app/controllers/question_votes_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsVotesController < ApplicationController
+  def create
+  	if param[:tipo]
+  		voto=QuestionsVotes.new(user_id:session[:user_id],question_id:param[:question_id],tipo:param[:tipo])
+  	else
+  		voto=QuestionsVotes.new(user_id:session[:user_id],question_id:param[:question_id],tipo:param[:tipo])
+  	end
+  	if voto.save
+  	
+  	else
+  		redirect_to root_path
+  	end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   get 'labels/index'
 
   post 'labels/create'
+  post 'question_votes/create'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
