3a9d6c24829621a7ef557d23bda8ddb805f6d287
AlarconZQL <ezealarcon@outlook.com>
2017-11-18 05:01:31 -0300

    Modificacion a respuestas

--- app/controllers/answers_comments_controller.rb
+++ app/controllers/answers_comments_controller.rb
@@ -1,14 +1,15 @@
 class AnswersCommentsController < ApplicationController
-def create
-  comentario = params[:comentario]
-  answer_id = params[:answer_id]
-  question_id = params[:question_id]
+  def create
+    comentario = params[:comentario]
+    answer_id = params[:answer_id]
+    question_id = params[:question_id]
 
-  if comentario != ""
-    c = AnswerComment.new(contenido: comentario, question_id: answer_id, user_id: session[:user_id])
-    if c.save == false
-      flash[:message]="Error al guardar el comentario"
+    if comentario != ""
+      c = AnswerComment.new(contenido: comentario, answer_id: answer_id, user_id: session[:user_id])
+      if c.save == false
+        flash[:message]="Error al guardar el comentario"
+      end
     end
+    redirect_to "#{questions_path}/#{question_id}"
   end
-  redirect_to "#{questions_path}/#{question_id}"
 end
